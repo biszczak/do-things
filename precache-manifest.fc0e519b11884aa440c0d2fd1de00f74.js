ed5561232fb57f9509f275c707ae966a608a7825
biszczak <dbiszczak@gmail.com>
2019-07-19 10:16:57 +0200

    Updates

---
+++ precache-manifest.fc0e519b11884aa440c0d2fd1de00f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7def6c41f41465141bf3db00d35f4d",
+    "url": "/do-things/index.html"
+  },
+  {
+    "revision": "639db8c79eb5f2494579",
+    "url": "/do-things/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "c0e30d0e54f6be3b678f",
+    "url": "/do-things/static/css/main.cbf960c0.chunk.css"
+  },
+  {
+    "revision": "639db8c79eb5f2494579",
+    "url": "/do-things/static/js/2.aebd44e2.chunk.js"
+  },
+  {
+    "revision": "c0e30d0e54f6be3b678f",
+    "url": "/do-things/static/js/main.e2a0a0e4.chunk.js"
+  },
+  {
+    "revision": "5869e01faf71a5c06138",
+    "url": "/do-things/static/js/runtime~main.a455d1f7.js"
+  },
+  {
+    "revision": "c7e149362470f2e78bc915e610fc133a",
+    "url": "/do-things/static/media/background.c7e14936.jpg"
+  }
+]);
